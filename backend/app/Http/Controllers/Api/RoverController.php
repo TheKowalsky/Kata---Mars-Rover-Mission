bcfce2bf1a7191045e9e0ae68247639f54ba14ec
Lluis Gallart Cid <lluis.gallart@estudiants.urv.cat>
2025-12-24 18:26:28 +0100

    Backend: Implementacio comand (Rover ja es mou correctament)

--- backend/app/Http/Controllers/Api/RoverController.php
+++ backend/app/Http/Controllers/Api/RoverController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\Rover\RoverEngine;     // És necesari per enviar les dades a la funcio que tractara les dades del command
+
 
 class RoverController extends Controller
 {
@@ -16,6 +18,55 @@ class RoverController extends Controller
             'x' => $rover->x,
             'y' => $rover->y,
             'direction' => $rover->direction,
+        ]);
+    }
+    /* Funció POST on el usuari enviara les comandes que vol que faci el seu rover, en aquest cas 
+    es comprobara que tota la comanda s'ha introduit correctament*/  
+   public function commands(Request $request, RoverEngine $engine)
+    {
+        // Validació (i aquí és on tu ja fas strtoupper)
+        $data = $request->validate([
+            'commands' => ['required', 'string'],
+        ]);
+        // Es possen tota la cadena de comandes en majuscules
+        $commands = strtoupper($data['commands']);
+
+        // Bucle que tracta tots els caracters per veure que son caracters valids
+        if (!preg_match('/^[FLR]+$/', $commands)) {
+            return response()->json([
+                'message' => 'La cadena de comandes té valors incorrectes (sol F, L, R).',
+            ], 422);
+        }
+
+        // Obtenim el rover de l’usuari autenticat
+        $rover = $request->user()->rover;
+
+        // Obtenim el estat inicial per després poder-ho veure amb mes detall al Postman
+        $start = [
+            'x' => (int) $rover->x,
+            'y' => (int) $rover->y,
+            'direction' => (string) $rover->direction,
+        ];
+
+        // Executem RoverEngine el qual es la funcio amb tota la logica
+        $result = $engine->run(
+            x: $start['x'],
+            y: $start['y'],
+            direction: $start['direction'],
+            commands: $commands
+        );
+
+        // Mostrem el estat final
+        $rover->update($result['end']);
+
+        // Retornem els "steps" al usuari, mes endabant al frontend
+        return response()->json([
+            'start' => $start,
+            'steps' => $result['steps'],
+            'end' => $result['end'],
+            'status' => $result['status'],
+            'obstacle' => $result['obstacle'],
+            'commands' => $commands,
         ]);
     }
 }
---
+++ backend/app/Services/Rover/RoverEngine.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Services\Rover;
+
+/*Funció logica que retorna un "buffer" que mostrara pas per pas el moviment del rover
+es fa d'aquesta manera per poder veure de forma "fluida" com es mou el rover, sino sol veuriem el estat inicial i el final.
+Aquesta funcio no accedira MAI a la Base de Dades*/
+final class RoverEngine
+{
+    public function run(int $x, int $y, string $direction, string $commands): array
+    {
+        $dir = strtoupper($direction);                                          // Obtenim la direccio actual (Nord(N), Est(E), Oest(W), Sud(S))
+        $commands_array = str_split($commands);                                 // Tranformem el String en un array de caracters per tractar acada caracter per separat
+        $direccions = ['N', 'E', 'S', 'W'];                                     // La metodologia utilitzada es la següient --> s'ha utilitzat una mena de vector circular, quan arribem al final del vector tornem a començar directament,
+                                                                                // aixi el unic que ha de fer les condicions es sumar o resta aquest valor depenende la direccio i obtenim la nova direccio sense tenir que usar mes logica.
+       
+        $steps = [];                                                            //L'objectiu es emmagatzema cada moviment del rover, ja que despres es vol fer un frontend que mostri un moviment dinamic d'aquest rover
+
+        foreach ($commands_array as $c) {                                       // Bucle que tracta cada caracter del array
+            if ($c === 'L') {                                                   // En cas L --> girar a esquerra
+                $index = array_search($dir, $direccions, true);                 // Obtenim el index
+                if ($index === 0){                                              // Com hem de restar 1, en cas de ser el inici del vector hem de situar-nos en la ultima poscio
+                    $index = 3; 
+                }else{                                                          // En cas contrari sol hem de restar 1 al index
+                    $index--;
+                }
+                $dir = $direccions[$index];                                     // Obtenim la direcció actualitzada
+            } elseif ($c === 'R') {                                             // La logica es el mateix que en el cas anterior per ara hem de sumar i en cas d'arribar al final del vector, posar el index a 0
+                $index = array_search($dir, $direccions, true);
+                if($index === 3){
+                    $index = 0;
+                }else{
+                    $index++;
+                }
+                $dir = $direccions[$index];
+            } elseif ($c === 'F') {                                             // En cas de que l'acció sigui avançar:
+                [$x, $y] = $this->finsEndavant($x, $y, $dir);                   // Cridem directament a una funcio, aquesta funcio ens retorna directament la nova posició X i Y
+            } else {
+                throw new \InvalidArgumentException("Moviment incorrecte: {$c}");   // En cas d'error, tornem el error
+            }
+
+            // Guardem snapshot després d'executar la comanda
+            $steps[] = [
+                'cmd' => $c,
+                'x' => $x,
+                'y' => $y,
+                'direction' => $dir,
+            ];
+        }
+        // Retornem al RoverController totes les dades obtingudes
+        return [
+            'end' => ['x' => $x, 'y' => $y, 'direction' => $dir],
+            'steps' => $steps,
+            'status' => 'completed',
+            'obstacle' => null,
+        ];
+    }
+
+    private function finsEndavant(int $x, int $y, string $dir): array
+    {
+        // Mirem acada possibilitat, i depenent de com estigui mirant el rover haruem de sumar o restar la X o la Y
+        return match ($dir) {
+            'N' => [$x, $this->wrap($y + 1)],
+            'S' => [$x, $this->wrap($y - 1)],
+            'E' => [$this->wrap($x + 1), $y],
+            'W' => [$this->wrap($x - 1), $y],
+            default => throw new \InvalidArgumentException("Direccions invalides: {$dir}"),
+        };
+    }
+    // En cas de que el rover surti del terra 200 x 200 el que fem es que torni a entrar pel costat contrari
+    private function wrap(int $value): int
+    {
+        $size = 200;
+        $m = $value % $size;
+        return $m < 0 ? $m + $size : $m;
+    }
+
+}
